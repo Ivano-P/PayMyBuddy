7c852f1e16a717760f7d030cc4643b558b2dc46f
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-07-09 11:19:51 +0200

    added error popup for contact form when trying to add contact that doesnt exist

--- src/main/java/com/paymybuddy/paymybuddy/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/paymybuddy/paymybuddy/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,18 @@
 package com.paymybuddy.paymybuddy.exceptions;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException ex, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMsg", "Contact not found");
+        return "redirect:/contact";
+    }
 }
